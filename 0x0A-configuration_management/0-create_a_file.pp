ff2c72f135970613146a500b8816bb2a5f42aa91
Amanuel Wubete <matiowsshimelis@Amanuels-MacBook-Air.local>
2022-09-19 21:08:29 +0300

    0x0A

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-# creates a file in the /tmp directory
-file { '/tmp/holberton':
-  ensure  => 'file',
+# Create a file in /tmp
+
+file { 'school':
+  content => 'I love Puppet',
+  group   => 'www-data',
   mode    => '0744',
   owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
+  path    => '/tmp/school',
 }
